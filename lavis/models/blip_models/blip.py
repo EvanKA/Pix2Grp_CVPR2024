458fd465bba4d94d2b606e1b85f50ec1ef1f5bf2
EvanKA <e0337884@u.nus.edu>
2025-01-07 14:54:15 +0700

    chg bert-base-uncased to use dyn dir

--- lavis/models/blip_models/blip.py
+++ lavis/models/blip_models/blip.py
@@ -21,7 +21,7 @@ logger = logging.getLogger("lavis")
 class BlipBase(BaseModel):
     @classmethod
     def init_tokenizer(cls):
-        tokenizer = BertTokenizer.from_pretrained("/public/home/lirj2/projects/LAVIS_GITM/data/bert-base-uncased", local_files_only=True)
+        tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", local_files_only=True)
         tokenizer.add_special_tokens({"bos_token": "[DEC]"})
         tokenizer.add_special_tokens({"additional_special_tokens": ["[ENC]"]})
         tokenizer.enc_token_id = tokenizer.additional_special_tokens_ids[0]
